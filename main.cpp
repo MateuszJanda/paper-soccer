a0e00d8efe0435afff98a624f8c95b8f8f2e9170
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-15 12:27:29 +0200

    Added test skeleton

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+#include "gtest/gtest.h"
 #include <iostream>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
