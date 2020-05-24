d970c0c51c0cf0ae85c4c7a254c8a83a1f0dc9e1
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-24 19:06:48 +0200

    All key handling

--- src/Game.cpp
+++ src/Game.cpp
@@ -17,10 +17,38 @@ void Game::run()
     {
         int c = m_ncurses.getChar();
         switch(c) {
-            case 'q':
-                m_board.moveBall(Direction::TopLeft);
-                m_view.drawBoard();
-                break;
+        case 'q':
+            m_board.moveBall(Direction::TopLeft);
+            m_view.drawBoard();
+            break;
+        case 'w':
+            m_board.moveBall(Direction::Top);
+            m_view.drawBoard();
+            break;
+        case 'e':
+            m_board.moveBall(Direction::TopRight);
+            m_view.drawBoard();
+            break;
+        case 'a':
+            m_board.moveBall(Direction::Left);
+            m_view.drawBoard();
+            break;
+        case 'd':
+            m_board.moveBall(Direction::Right);
+            m_view.drawBoard();
+            break;
+        case 'z':
+            m_board.moveBall(Direction::BottomLeft);
+            m_view.drawBoard();
+            break;
+        case 'x':
+            m_board.moveBall(Direction::Bottom);
+            m_view.drawBoard();
+            break;
+        case 'c':
+            m_board.moveBall(Direction::BottomRight);
+            m_view.drawBoard();
+            break;
         }
     }
 }
