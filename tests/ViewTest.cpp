97844193c5d53a0e589d41f3ce67faec1e8ecede
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-24 18:07:49 +0200

    Added drawCell tests

--- tests/ViewTest.cpp
+++ tests/ViewTest.cpp
@@ -16,12 +16,15 @@ const std::set<Direction> TLEFT{Direction::TopLeft};
 const std::set<Direction> RTRIGHT{Direction::TopRight, Direction::Right};
 const std::set<Direction> RIGHT{Direction::Right};
 
+const std::size_t WIDTH{9};
+const std::size_t HEIGHT{13};
+
 const std::size_t TOP_NET_LINE{0};
-const std::size_t BOTTOM_NET_LINE{12};
+const std::size_t BOTTOM_NET_LINE{HEIGHT-1};
 const std::size_t TOP_BORDER_LINE{TOP_NET_LINE+1};
 const std::size_t BOTTOM_BORDER_LINE{BOTTOM_NET_LINE-1};
 const std::size_t CENTER_LINE{6};
-const std::size_t RIGHT_LINE{8};
+const std::size_t RIGHT_LINE{WIDTH-1};
 const std::size_t GOALPOST_LEFT{3};
 const std::size_t GOALPOST_RIGHT{5};
 }
@@ -34,8 +37,8 @@ public:
     {
         EXPECT_CALL(boardMock, getGoalpostLeft()).WillRepeatedly(Return(GOALPOST_LEFT));
         EXPECT_CALL(boardMock, getGoalpostRight()).WillRepeatedly(Return(GOALPOST_RIGHT));
-        EXPECT_CALL(boardMock, getHeight()).WillRepeatedly(Return(BOTTOM_NET_LINE+1));
-        EXPECT_CALL(boardMock, getWidth()).WillRepeatedly(Return(RIGHT_LINE+1));
+        EXPECT_CALL(boardMock, getHeight()).WillRepeatedly(Return(HEIGHT));
+        EXPECT_CALL(boardMock, getWidth()).WillRepeatedly(Return(WIDTH));
     }
     StrictMock<BoardMock> boardMock;
     StrictMock<NCursesMock> ncursesMock;
@@ -133,7 +136,20 @@ TEST_F(ViewTest, checkFilterDirsForRightLineWhenThisLine)
     ASSERT_EQ(neighSkip, EMPTY);
 }
 
-TEST_F(ViewTest, drawCell)
+TEST_F(ViewTest, checkDrawCellPlusMarkerSkipAllDirs)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::Right, Direction::TopRight};
+    Skip neighSkip{Direction::TopLeft};
+
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 + 2;
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellPlusMarkerNoNeighbour)
 {
     Position p{0,0};
     Skip nodeSkip{Direction::Right, Direction::TopRight};
@@ -141,10 +157,170 @@ TEST_F(ViewTest, drawCell)
 
     EXPECT_CALL(boardMock, hasNeighbour(p, Direction::Top)).WillOnce(Return(false));
     int x = p.x * 3 + 2;
-    int y = p.x * 2 + 2;
+    int y = p.y * 2 + 2;
     EXPECT_CALL(ncursesMock, print(x, y, "+"));
 
     view.drawCell(p, nodeSkip, neighSkip);
 }
+
+TEST_F(ViewTest, checkDrawCellTopPath)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Right, Direction::TopRight};
+    Skip neighSkip{Direction::TopLeft};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::Top)).WillOnce(Return(true));
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 -1 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "|"));
+    x = p.x * 3 + 2;
+    y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellRightPathNoNeighbour)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top,Direction::TopRight};
+    Skip neighSkip{Direction::TopLeft};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::Right)).WillOnce(Return(false));
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellRightPath)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::TopRight};
+    Skip neighSkip{Direction::TopLeft};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::Right)).WillOnce(Return(true));
+    int x = p.x * 3 + 1 + 2;
+    int y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "--"));
+    x = p.x * 3 + 2;
+    y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellTopRightPathNoNeighbour)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::Right};
+    Skip neighSkip{Direction::TopLeft};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::TopRight)).WillOnce(Return(false));
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellTopRightPath)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top,Direction::Right};
+    Skip neighSkip{Direction::TopLeft};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::TopRight)).WillOnce(Return(true));
+    int x = p.x * 3 + 1 + 2;
+    int y = p.y * 2 - 1 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "/"));
+    x = p.x * 3 + 2;
+    y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellTopLeftPathNeighbourOutOfRange)
+{
+    Position p{WIDTH-1,0};
+    Skip nodeSkip{Direction::Top, Direction::Right, Direction::TopRight};
+    Skip neighSkip{};
+
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellTopLeftPathNoNeighbour)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::Right, Direction::TopRight};
+    Skip neighSkip{};
+
+    Position p1{1, 0};
+    EXPECT_CALL(boardMock, hasNeighbour(p1, Direction::TopLeft)).WillOnce(Return(false));
+    int x = p.x * 3 + 2;
+    int y = p.y * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+TEST_F(ViewTest, checkDrawCellTopLeftPath)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::Right, Direction::TopRight};
+    Skip neighSkip{};
+
+    Position p1{1, 0};
+    EXPECT_CALL(boardMock, hasNeighbour(p1, Direction::TopLeft)).WillOnce(Return(true));
+    int x = p1.x * 3 -1 + 2;
+    int y = p1.y * 2 -1 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "\\"));
+    x = p.x * 3 + 2;
+    y = p.x * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
+
+TEST_F(ViewTest, checkDrawCellCrossPath)
+{
+    Position p{0,0};
+    Skip nodeSkip{Direction::Top, Direction::Right};
+    Skip neighSkip{};
+
+    EXPECT_CALL(boardMock, hasNeighbour(p, Direction::TopRight)).WillOnce(Return(true));
+    Position p1{1, 0};
+    EXPECT_CALL(boardMock, hasNeighbour(p1, Direction::TopLeft)).WillOnce(Return(true));
+    int x = p.x * 3 + 1 + 2;
+    int y = p.y * 2 - 1 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "><"));
+    x = p.x * 3 + 2;
+    y = p.x * 2 + 2;
+
+    EXPECT_CALL(ncursesMock, print(x, y, "+"));
+
+    view.drawCell(p, nodeSkip, neighSkip);
+}
+
 
 }
