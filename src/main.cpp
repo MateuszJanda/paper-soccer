06b6d67c7e089b5c5054a3cf3d561d499acea905
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-27 22:23:21 +0200

    Simple server

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,64 @@
 
 using namespace std;
 
+using boost::asio::ip::tcp;
+
+class Server
+{
+public:
+    Server(boost::asio::io_service& io_service, const tcp::endpoint& endpoint)
+        : m_acceptor(io_service, endpoint),
+          m_socket(io_service)
+    {
+        accept();
+    }
+
+    void accept()
+    {
+        m_acceptor.async_accept(m_socket,
+            [this](boost::system::error_code ec)
+            {
+                if (!ec)
+                {
+//                    std::make_shared<chat_session>(std::move(m_socket), room_)->start();
+                    std::cout << "accept" << "\n";
+                }
+            });
+    }
+
+
+private:
+    tcp::acceptor m_acceptor;
+    tcp::socket m_socket;
+};
+
+
+
 int main(int argc, char** argv)
 {
     using namespace PaperSoccer;
-    Board b{8, 10};
 
-    NCurses nn;
-//    nn.getChar();
-    View v{b, nn};
-    v.drawBoard();
+    try
+    {
+        boost::asio::io_service io_service;
+        tcp::endpoint endpoint(tcp::v4(), 8787);
 
-    Game game{b, nn, v};
-    game.run();
+        Server server{io_service, endpoint};
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+      std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+//    Board b{8, 10};
+
+//    NCurses nn;
+////    nn.getChar();
+//    View v{b, nn};
+//    v.drawBoard();
+
+//    Game game{b, nn, v};
+//    game.run();
 }
